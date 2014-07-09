f027de957b3a92840f83974844555844023fdd11
marcoberri <marcoberri@gmail.com>
2014-07-09 15:02:08 +0200

    end 4 plot

--- plot4.R
+++ plot4.R
@@ -8,7 +8,7 @@ download_file<-function(folder, file_result){
       ,file_result, sep = "", collapse = NULL)
   )
   ) {
-    printUser("Start download File")	
+    printUser("Start download File")  
     dir.create(paste ("./" , folder, sep = "", collapse = NULL))
     fileUrl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip" 
     download.file(fileUrl,destfile=paste ( folder, "/power_consumption.zip", sep = "", collapse = NULL), method="curl")
@@ -32,11 +32,24 @@ fulldata_subset <- fulldata[ which(fulldata$Date=='2007-02-01' | fulldata$Date==
 
 fulldata_subset$Day_of_Week <- strftime(fulldata_subset$Date,'%a')
 
-png(filename = "./plot3.png", width = 480, height = 480, units = "px",bg="white")
+png(filename = "./plot4.png", width = 800, height = 800, units = "px",bg="white")
+
+par (mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
+
+plot(fulldata_subset$DateTime,fulldata_subset$Global_active_power, ylab='Global Active Power (kilowatts)', xlab='', type='l')
+
+
+plot(fulldata_subset$DateTime,fulldata_subset$Voltage, ylab='Voltage', xlab='datetime', type='l')
+
+
 plot(fulldata_subset$DateTime,fulldata_subset$Sub_metering_1, ylab='Energy sub metering', col="black", xlab='', type='l')
 lines(fulldata_subset$DateTime,fulldata_subset$Sub_metering_2, type="l", col="red")
 lines(fulldata_subset$DateTime,fulldata_subset$Sub_metering_3, type="l", col="blue")
-legend("topright", lty=1, lwd=1, col=c("black","blue","red"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+legend("topright", lty=1, lwd=1, col=c("black","blue","red"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty="n")
+
+
+plot(fulldata_subset$DateTime,fulldata_subset$Global_reactive_power, ylab='Global_reactive_power', xlab='datetime', type='l')
+
+
 
 dev.off()
-
